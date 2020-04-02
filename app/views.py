20352fb43522d56b7697e278e8738287760fba2e
Oleg <moxxxie07@gmail.com>
2020-04-02 20:45:49 +0300

    add including question instead of 'hardcoding'

--- app/views.py
+++ app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 def hello(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {
+        'questions': range(10),
+    })
 # Create your views here.
