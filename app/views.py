6c087619784d7aff5f2256e4a819322995bd64e6
Oleg <moxxxie07@gmail.com>
2020-04-04 12:13:46 +0300

    add hot, tag pages and pagination bar

--- app/views.py
+++ app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.core.paginator import Paginator
+
+
+def paginate(objects_list, request, per_page=10):
+    #do smth with Paginator, etc...
+    paginator = Paginator(objects_list, per_page)
+    page = request.GET.get('page', 1)
+    objects_page = paginator.get_page(page)
+    return objects_page
+    
+questions = []
+for i in range(1, 30):
+    questions.append({
+        'title': 'title' + str(i),
+        'id': i,
+        'text': 'text' + str(i)
+    })
 
 def index(request):
     return render(request, 'index.html', {
@@ -18,17 +35,20 @@ def settings(request):
 def ask(request):
     return render(request, 'ask.html', {})
 
-def question_page(request):
-    return render(request, 'question_page.html', {
-        'answers': range(3)
+def question(request, qid):
+    question = questions[qid]
+    return render(request, 'question.html', {
+        'question': question 
     })
 
-def tag(request):
+def tag(request, tag):
     return render(request, 'tag_questions.html', {
-        'tag': 'bender',
+        'tag': tag,
         'questions': range(10),
     }
 )
 
-
-    
+def hot(request):
+    return render(request, 'hot.html', {
+        'questions': range(5)
+    })
--- askme/urls.py
+++ askme/urls.py
@@ -21,10 +21,13 @@ from app import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('ask/', views.ask, name='ask'),
     path('settings/', views.settings, name='settings'),
-    path('question/1/', views.question_page, name='question_page'),
-    path('tag/bender/', views.tag, name='tag'),
+    path('ask/', views.ask, name='ask'),
+
+    path('question/<int:qid>/', views.question, name='question'),
+    path('tag/<str:tag>/', views.tag, name='tag'),
 ]
