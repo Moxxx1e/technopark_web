4b01a622c5d4ba1bfc16770868d0223824d4b355
Oleg <moxxxie07@gmail.com>
2020-04-04 13:17:32 +0300

    add pagination

--- app/views.py
+++ app/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.core.paginator import Paginator
 
+gl_per_page = 10
 
-def paginate(objects_list, request, per_page=10):
-    #do smth with Paginator, etc...
+def paginate(objects_list, request, per_page=gl_per_page):
     paginator = Paginator(objects_list, per_page)
-    page = request.GET.get('page', 1)
-    objects_page = paginator.get_page(page)
-    return objects_page
     
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
+
 questions = []
-for i in range(1, 30):
+for i in range(1, 31):
     questions.append({
         'title': 'title' + str(i),
         'id': i,
@@ -20,7 +21,8 @@ for i in range(1, 30):
 
 def index(request):
     return render(request, 'index.html', {
-        'questions': range(10),
+        'questions': range(gl_per_page),
+        'page_obj': paginate(questions, request)
     })
 
 def login(request):
@@ -36,7 +38,7 @@ def ask(request):
     return render(request, 'ask.html', {})
 
 def question(request, qid):
-    question = questions[qid]
+    question = questions[qid - 1]
     return render(request, 'question.html', {
         'question': question 
     })
@@ -44,11 +46,13 @@ def question(request, qid):
 def tag(request, tag):
     return render(request, 'tag_questions.html', {
         'tag': tag,
-        'questions': range(10),
+        'questions': range(gl_per_page),
+        'page_obj': paginate(questions, request)
     }
 )
 
 def hot(request):
     return render(request, 'hot.html', {
-        'questions': range(5)
+        'questions': range(gl_per_page),
+        'page_obj': paginate(questions, request)
     })
