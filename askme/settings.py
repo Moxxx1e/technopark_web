2fdfcf6e5918ad7d9b5d449abc30c7d3f89d16ad
Oleg <moxxxie07@gmail.com>
2020-04-06 15:48:19 +0300

    set up env for postgresql db

--- askme/settings.py
+++ askme/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'askme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'askme',
+        'USER': 'askme',
+        'PASSWORD': 'askme',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
